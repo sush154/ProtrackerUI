b11cf2ae7dd640bda9b3c2b8339e3a3de007d3a3
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-22 17:39:35 +0530

    Authentication

--- app/module/dashboard/dashboard.component.ts
+++ app/module/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProjectProvider} from '../../providers/project/project.provider';
-
+import {Router} from '@angular/router';
 import {Project} from '../../model/project';
 
 
@@ -15,12 +15,17 @@ export class DashboardComponent implements OnInit{
 
     private projectsList : any;
 
-    constructor(private projectProvider : ProjectProvider){}
+    constructor(private projectProvider : ProjectProvider, private router : Router){}
 
 
     getAllProjects() : void {
         let projects : any;
-        this.projectProvider.getAllProjects().then(res => this.projectsList = res);
+        this.projectProvider.getAllProjects().then(res => {
+            if(res.status === 401){
+                this.router.navigate(['/login']);
+            }
+            this.projectsList = res;
+        });
 
     }
 
