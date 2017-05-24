3a6c4657d6b3e9ddb72c5f424460146c1a8f30a0
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-24 19:39:24 +0530

    removing getAllProjects service call

--- app/module/dashboard/dashboard.component.ts
+++ app/module/dashboard/dashboard.component.ts
@@ -1,35 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {ProjectProvider} from '../../providers/project/project.provider';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
+
 import {Project} from '../../model/project';
 
 
 @Component({
     selector:       'dashboard',
     templateUrl:    './app/module/dashboard/dashboard.component.html',
-    providers:      [ProjectProvider],
     styleUrls:      ['./app/module/dashboard/dashboard.component.css']
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent{
 
-    private projectsList : any;
-
-    constructor(private projectProvider : ProjectProvider, private router : Router){}
-
-
-    getAllProjects() : void {
-        let projects : any;
-        this.projectProvider.getAllProjects().then(res => {
-            if(res.status === 401){
-                this.router.navigate(['/login']);
-            }
-            this.projectsList = res;
-        });
-
-    }
-
-    ngOnInit(): void {
-        this.getAllProjects();
-    }
 }
