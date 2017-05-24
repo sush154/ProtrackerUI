adf11d0004371df6f6eb6e7e3c91e945038a5a22
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-24 19:37:35 +0530

    Adding getAllProjects on load

--- app/module/dashboard/dashboard-projects.component.ts
+++ app/module/dashboard/dashboard-projects.component.ts
@@ -1,14 +1,35 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Project} from '../../model/project';
+import {ProjectProvider} from '../../providers/project/project.provider';
+import {Router} from '@angular/router';
 
 
 @Component({
     selector: 'dashboard-projects',
     templateUrl: './app/module/dashboard/dashboard-projects.html',
-    styleUrls:      ['./app/module/dashboard/dashboard.component.css']
+    styleUrls:      ['./app/module/dashboard/dashboard.component.css'],
+    providers:      [ProjectProvider]
 })
 
-export class DashboardProjects {
+export class DashboardProjects implements OnInit{
 
-    @Input() projectsList : Project[];
+
+    private projectsList : any;
+
+    constructor(private projectProvider : ProjectProvider, private router : Router){}
+
+    getAllProjects() : void {
+
+        this.projectProvider.getAllProjects().then(res => {
+            if(res.status === 401){
+                this.router.navigate(['/login']);
+            }
+            this.projectsList = res.projects;
+        });
+
+    }
+
+    ngOnInit(): void {
+        this.getAllProjects();
+    }
 }
