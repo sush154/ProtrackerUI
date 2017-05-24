566172b9469b9f417442f265336e108f73b87e75
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-24 19:38:35 +0530

    Adding getAllTasks on load and expired project color

--- app/module/dashboard/dashboard-tasks.component.ts
+++ app/module/dashboard/dashboard-tasks.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {TaskProvider} from '../../providers/task/task.provider';
+
 
 @Component({
     selector: 'dashboard-tasks',
-    templateUrl: './app/module/dashboard/dashboard-tasks.html'
+    templateUrl: './app/module/dashboard/dashboard-tasks.html',
+    styleUrls:      ['./app/module/dashboard/dashboard.component.css'],
+    providers:  [TaskProvider]
 })
 
-export class DashboardTasks {
+export class DashboardTasks implements OnInit{
 
+    private taskList : any;
+
+    constructor(private taskService : TaskProvider, private router : Router){}
+
+    getAllTasks() : void {
+        this.taskService.getAllTasks()
+        .then((res) => {
+            this.taskList = res.tasks;
+        });
+    }
+
+    isInDanger(expectedComDate, taskStatus): boolean{
+
+        var parts = expectedComDate.split("T")[0].split("-");
+        if(new Date(parts[0], parts[1]-1, parts[2]).setHours(0,0,0,0) < new Date().setHours(0,0,0,0)){
+            return true;
+        }
+
+        return false;
+    }
+
+    ngOnInit() : void {
+        this.getAllTasks();
+    }
 }
