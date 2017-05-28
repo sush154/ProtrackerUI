f54532a0d55e33bc8acb8432fe1665210ba1f0dd
SushantJoshi-Cognizant <Sushant.Joshi@cognizant.com>
2017-05-28 22:43:04 +0530

    Task Component Styling

--- app/module/tasks/tasks.component.ts
+++ app/module/tasks/tasks.component.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-tasks',
-    templateUrl: './app/module/tasks/tasks.component.html'
+    templateUrl: './app/module/tasks/tasks.component.html',
+    styleUrls: ['./app/module/tasks/task.component.css']
 })
 
 export class TasksComponent{
-
+    
 }
