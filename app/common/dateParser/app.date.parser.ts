21680643bb101fe9f32c5a557543e7652a3d7b3b
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-05 19:49:10 +0530

    Custom Date Parser

---
+++ app/common/dateParser/app.date.parser.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'dateParser'
+})
+
+export class DateParser implements PipeTransform{
+    transform(record : any) {
+
+        if((typeof record === 'string') && record !== undefined){
+            let fullDate = record.split("T")[0];
+
+            let yr = fullDate.split("-")[0];
+            let mn = fullDate.split("-")[1];
+            let dt = fullDate.split("-")[2];
+
+            return dt + "/" + mn + "/" + yr;
+        }
+    }
+}
