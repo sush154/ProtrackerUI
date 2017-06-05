591aae2d03bdaca29b9de9131d9ed5bb13f72f3b
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-05 19:51:32 +0530

    Adding Date Parser Module

--- app/module/projects/projects.module.ts
+++ app/module/projects/projects.module.ts
@@ -2,14 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {AppHeaderModule} from '../appHeader/header.module';
+import {HelpModule} from '../helpModal/app.help.module';
 import {ProjectsComponent} from './projects.component';
+import {ProjectDetailsComponent} from './project.details.component';
 import {ProjectSortModule} from '../../common/sort/project/project.sort.module';
+import {DateParserModule} from '../../common/dateParser/app.date.parser.module';
 import {ToasterModule, ToasterService} from 'angular2-toaster';
 import {MyDatePickerModule} from 'mydatepicker';
 
 @NgModule({
-    imports:        [AppHeaderModule, CommonModule, ProjectSortModule, ToasterModule, FormsModule, MyDatePickerModule],
-    declarations:   [ProjectsComponent],
+    imports:        [AppHeaderModule, CommonModule, ProjectSortModule, ToasterModule,
+                        FormsModule, MyDatePickerModule,HelpModule,
+                        DateParserModule],
+    declarations:   [ProjectsComponent, ProjectDetailsComponent],
     providers:      [],
     exports:        [ProjectsComponent]
 })
