08c9fb76db657619895c8da67a467701fa94950a
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-16 18:22:26 +0530

    Adding Logout Provider

--- app/module/appHeader/header.component.ts
+++ app/module/appHeader/header.component.ts
@@ -2,11 +2,13 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {CommonAppService} from '../../sharedService/app.common-service';
+import {LogoutProvider} from '../../providers/logout/logout.provider';
 
 @Component({
     selector: 'app-header',
     templateUrl: './app/module/appHeader/header.component.html',
-    styleUrls:  ['./app/module/appHeader/app.header.style.css']
+    styleUrls:  ['./app/module/appHeader/app.header.style.css'],
+    providers: [LogoutProvider]
 })
 
 export class AppHeaderComponent implements OnInit{
@@ -16,16 +18,22 @@ export class AppHeaderComponent implements OnInit{
 
     constructor(private router : Router,
                 private location : Location,
-                private commonService : CommonAppService){}
+                private commonService : CommonAppService,
+                private logoutProvider : LogoutProvider){}
 
     navigate(pageName: string) : void {
         let navigationPage = "/"+pageName;
         this.router.navigate([navigationPage]);
     }
 
+    logout() : void {
+        this.logoutProvider.logout().then(res => {
+            this.router.navigate(['/login']);
+        });
+    }
+
     ngOnInit() : void {
         this.selectedPage = this.location.path().split("/")[1];
-
     }
 
     toggleMenu() : void {
