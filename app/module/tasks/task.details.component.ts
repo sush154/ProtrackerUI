10f8ad954357d271ef363ef6d40a4f77ebede65d
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-16 18:18:59 +0530

    Task Details Component

---
+++ app/module/tasks/task.details.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'task-details',
+    templateUrl : './app/module/tasks/tasks.details.component.html',
+    styleUrls: ['./app/module/tasks/task.component.css']
+})
+
+export class TaskDetailsComponent {
+    private a : any = [1,2,3,4,5,6];
+}
