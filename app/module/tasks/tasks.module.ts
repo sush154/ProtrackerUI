286ad4f47e1cf908e6fe77cd35f9c46b1eb5bdb2
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-16 18:26:49 +0530

    Task Details popup

--- app/module/tasks/tasks.module.ts
+++ app/module/tasks/tasks.module.ts
@@ -1,15 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 import {TasksComponent} from './tasks.component';
+import {TaskDetailsComponent} from './task.details.component';
 import {AppHeaderModule} from '../appHeader/header.module';
 import {CriticalitySortModule} from '../../common/sort/criticality/criticality.sort.module';
+import {DateParserModule} from '../../common/dateParser/app.date.parser.module';
+import {ToasterModule, ToasterService} from 'angular2-toaster';
+import { MyDatePickerModule } from 'mydatepicker';
 
 @NgModule({
-    imports:        [AppHeaderModule,CriticalitySortModule,CommonModule],
-    declarations:   [TasksComponent],
+    imports:        [AppHeaderModule,CriticalitySortModule,CommonModule,FormsModule,MyDatePickerModule,ToasterModule,DateParserModule],
+    declarations:   [TasksComponent, TaskDetailsComponent],
     providers:      [],
-    exports:        [TasksComponent]
+    exports:        [TasksComponent, TaskDetailsComponent]
 })
 
 export class TasksModule{}
