4547789a516d6330b13a390dd42644d699b5ddfc
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-22 18:02:07 +0530

    Constant array for criticality

---
+++ app/sharedService/app.criticality.ts
@@ -0,0 +1,7 @@
+export const CRITICALITY = [
+    {"id" : 1 , "value" : "Show Stopper"},
+    {"id" : 2 , "value" : "Critical"},
+    {"id" : 3 , "value" : "Major"},
+    {"id" : 4 , "value" : "Medium"},
+    {"id" : 5 , "value" : "Minor"}
+]
