68ac8182d8a0b0c765eb4ccaab07f026574cffb0
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-22 18:01:51 +0530

    Constant array for task status

---
+++ app/sharedService/app.task.status.ts
@@ -0,0 +1,8 @@
+export const TASKSTATUS = [
+    {"id" : 0, "value" : "Not Started"},
+    {"id" : 1, "value" : "In design"},
+    {"id" : 2, "value" : "In Build"},
+    {"id" : 3, "value" : "In Test"},
+    {"id" : 4, "value" : "Completed"},
+    {"id" : 5, "value" : "Defective"}
+]
